701fec4f1f2b669144e31cd020a1832d172388d6
Andy Keck <agkeck.iivi@gmail.com>
2025-12-09 10:01:29 -0700

    refactor: Adjust OLED symbol layout, pin positions, and associated labels in schematic.

--- main/station_data.c
+++ main/station_data.c
@@ -7,6 +7,10 @@ station_t radio_stations[] = {
     {"KDUR", "Durango", "https://kdurradio.fortlewis.edu/stream", CODEC_TYPE_MP3},
     {"KOTO", "Telluride", "http://26193.live.streamtheworld.com/KOTOFM.mp3", CODEC_TYPE_MP3},
     {"KHEN", "Salida", "https://stream.pacificaservice.org:9000/khen_128", CODEC_TYPE_MP3},
+    // try these:
+    // {"KRCL", "Salt Lake City", "http://stream.xmission.com:8000/krcl-high", CODEC_TYPE_AAC}, // 192kbps aac-lc
+    // {"KRCL", "Salt Lake City", "http://stream.xmission.com:8000/krcl-low", CODEC_TYPE_AAC}, // 56kbps aac plus
+    // didn't work:
     // {"KRCL", "Salt Lake City", "https://stream.xmission.com/krcl-high", CODEC_TYPE_AAC}, // 5 seconds only
     // {"KRCL", "Salt Lake City", "https://kcpw.xmission.com/kuaa", CODEC_TYPE_MP3},  // 5 seconds at a time
     {"KWSB", "Gunnison", "https://kwsb.streamguys1.com/live", CODEC_TYPE_MP3},
